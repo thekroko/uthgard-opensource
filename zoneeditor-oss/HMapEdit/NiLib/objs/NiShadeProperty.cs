1a9e104c568bdb367bf5a053acd1308cb651b9f7
DigitalBox98 <digitalbox098@gmail.com>
2023-08-14 00:10:56 +0200

    Add NiShadeProperty in order to load Tajendi2 zone correctly

---
+++ zoneeditor-oss/HMapEdit/NiLib/objs/NiShadeProperty.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace MNL
+{
+    public class NiShadeProperty : NiProperty
+    {
+        public ushort Flags;
+
+        public NiShadeProperty(NiFile file, BinaryReader reader)
+            : base(file, reader)
+        {
+            Flags = reader.ReadUInt16();
+        }
+    }
+}
